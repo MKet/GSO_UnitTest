1f5fd878cee6a320a3d997849da3023a49d450fc
Marco Ketelaars <marco-ketelaars94@hotmail.com>
2017-09-27 12:43:39 +0200

    appointment tests

--- src/fontys/time/AppointmentTest.java
+++ src/fontys/time/AppointmentTest.java
@@ -3,15 +3,53 @@ package fontys.time;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.*;
+
+/**
+ * @Author Marco Ketelaars
+ */
 public class AppointmentTest {
 
     Appointment appointment;
+
     @Before
     public void setup() {
-        TimeSpan span = new TimeSpan(new Time(1995, 3, 31, 15, 35), new Time(2017, 3, 5, 13, 30));
+        TimeSpan span = new TimeSpan(new Time(1995, 3, 31, 15, 35), new Time(2117, 3, 5, 13, 30));
         appointment = new Appointment("git gud", span);
     }
 
     @Test
-    public void addContact() { }
+    public void addContact() {
+        appointment.addContact( new Contact("Rick"));
+
+        assertTrue(appointment.invitees().hasNext());
+    }
+
+    @Test
+    public void addConflictContact() {
+        Contact contact = new Contact("Rick");
+
+
+        TimeSpan span = new TimeSpan(new Time(1995, 3, 31, 15, 35), new Time(2000, 3, 5, 13, 30));
+
+        contact.addAppointment(new Appointment(null, span));
+
+        appointment.addContact(contact);
+
+        assertTrue(appointment.invitees().hasNext());
+    }
+
+    @Test
+    public void removeContact() {
+        Contact contact = new Contact("Rick");
+        appointment.addContact(contact);
+        appointment.removeContact(contact);
+
+        assertFalse(appointment.invitees().hasNext());
+    }
+
+    @Test
+    public void getSubject() {
+        assertEquals("git gud", appointment.getSubject());
+    }
 }
